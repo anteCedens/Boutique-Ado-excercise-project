ec4294a275b763bab26cb49d36824e9dd447c66b
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-30 23:30:49 +0000

    Pasted some allauth apps to INSTALLED_APPS

--- boutique_ado_exercise_project/settings.py
+++ boutique_ado_exercise_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -68,13 +72,13 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    
+
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
+
 ]
 
 WSGI_APPLICATION = 'boutique_ado_exercise_project.wsgi.application'
