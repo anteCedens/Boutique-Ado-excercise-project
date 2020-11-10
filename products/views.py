79791e66162a7fcdb21ec9c2adf77e20d11879b9
anteCedens <jaksa.juric@yahoo.com.hr>
2020-11-10 02:31:29 +0000

    Set up if 'category' clause in views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,23 +10,31 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    categories = None
 
     # Checking whether request.get exists
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # this double underscore ("__") syntax is common when making queries in django
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         # Since we named the text input in the form 'q', we can just check if 'q' is in request.get
         if 'q' in request.GET:
             # If it is, set it equal to a variable called query
             query = request.GET['q']
             # If the query is blank it's not going to return any results
-            # So if that's the case let's use the Django messages framework 
-            # to attach an error message to the request. 
+            # So if that's the case let's use the Django messages framework
+            # to attach an error message to the request.
             # And then redirect back to the products url.
             if not query:
                 messages.error(
                     request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            # If the query isn't blank, use a special object from django.db.models called Q 
+            # If the query isn't blank, use a special object from django.db.models called Q
             # to generate a search query
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
@@ -45,7 +53,9 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product,
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/product_detail.html', context)
