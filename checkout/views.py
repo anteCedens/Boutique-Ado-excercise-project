91ec71366ff27e58b59ed7d33368ac2081b7df62
anteCedens <jaksa.juric@yahoo.com.hr>
2020-12-19 17:02:06 +0000

    Add checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -8,12 +8,14 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products')) # This will prevent people from manually accessing the URL by typing /checkout
+        return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hz79OBNKfgGBua74HhXEbsiuz3FVzNaqrGlv21i5s9nw9RMtnBdI7oUXxiwTr9dplWG3KSqbKlaLAnjDRg8dtUi00S99HD90a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
